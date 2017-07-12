dc4bc7290b68c0a7167d172ba3b0820c319490ad
Thu-Huynh <Thu-Huynh@users.noreply.github.com>
2017-07-12 08:12:03 +0700

    Add files via upload

---
+++ control.py
@@ -0,0 +1,60 @@
+import RPi.GPIO as GPIO
+import time
+import serial
+import os
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.output(17,1)
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1)
+while True:
+	if GPIO.input(2) == False:
+		GPIO.output(17,0)
+		time.sleep(1)
+		GPIO.output(17,1)
+		time.sleep(1)
+		
+		port.write('AT'+'\r\n')
+		rcv = port.read(10)
+		print rcv
+		time.sleep(0.1)
+		
+		port.write('ATE0'+'\r\n')
+		rcv = port.read(10)
+		print rcv
+		time.sleep(0.1)
+
+		port.write('AT+CMGF=1'+'\r\n')
+		rcv = port.read(10)
+		print rcv
+		time.sleep(0.1)
+
+		port.write('AT+CNMI=2,1,0,0,0'+'\r\n')
+		rcv = port.read(10)
+		print rcv
+		time.sleep(0.1)
+
+		port.write('AT+CMGS="01224976978"'+'\r\n')
+		rcv = port.read(10)
+		print rcv
+		time.sleep(0.1)
+
+		port.write('Raspberry Pi xin chao'+'\r\n')
+		rcv = port.read(10)
+		print rcv
+
+		port.write("\x1A")
+		time.sleep(10)
+		#for i in range(10):
+		#       rcv = port.read(10)
+		port.write('ATD01224976978;'+'\r\n')
+		rcv=port.read(10)
+		print rcv
+	
+		
+		
+		
+		
+		
+		
+		
